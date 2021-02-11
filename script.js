9a10bd0c4ad565b2b08f3916914c4fa1fd179f3c
SamantaSouza357 <samantasouza931@gmail.com>
2021-02-11 15:58:07 -0300

    calcular

---
+++ script.js
@@ -0,0 +1,34 @@
+class Calcular {
+
+    constructor() {
+        this.numbers = document.querySelectorAll(".number");
+        this.result = document.querySelector("#result");
+        this.btnReset = document.querySelector("#reset");
+        this.btnResolve = document.querySelector("#resolve");
+
+        this.btnReset.onclick = () => this.reset();
+        this.btnResolve.onclick = () => this.resolve();
+        this.insertNumber();
+    };
+
+    insertNumber() {
+        for (let n of this.numbers) {
+            n.onclick = () => {
+                // console.log(n);
+                console.log(n.value);
+                // this.result.value += n.value;
+            }
+        }
+    };
+
+    reset() {
+        this.result.value = "";
+    };
+
+    resolve() {
+        this.result.value != "" ? 
+        this.result.value = eval(this.result.value) : null;
+    };
+}
+
+const calculator = new Calcular;
